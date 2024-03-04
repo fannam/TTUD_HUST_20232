a61d7eacf539e13976d4eba5a02ca4489d6e212b
The Gardener <115874162+fannam@users.noreply.github.com>
2024-03-04 16:42:23 +0700

    Update ex1.2.cpp

--- buoi_1/ex1.2.cpp
+++ buoi_1/ex1.2.cpp
@@ -1,21 +1,13 @@
 #include <bits/stdc++.h>
-
 using namespace std;
+int A[1000][1000];
+bool visited[1000][1000];
 typedef struct position{
     int row, col, step;
 }position;
 queue<position>q;
-int main()
-{
-    int n, m, r, c;
-    cin>>n>>m>>r>>c;
-    int A[n][m];
-    bool visited[n][m];
-    for(int i=0; i<n; i++){
-        for(int j=0; j<m; j++){
-            cin>>A[i][j];
-        }
-    }
+
+int BFS(int m, int n, int r, int c){
     visited[r-1][c-1]=false;
     int dir_x[4]={0, 0, 1, -1};
     int dir_y[4]={1, -1, 0, 0};
@@ -24,8 +16,7 @@ int main()
         position cur_pos = q.front();
         q.pop();
         if(cur_pos.row==0||cur_pos.row==n-1||cur_pos.col==0||cur_pos.col==m-1){
-            printf("%d\n", cur_pos.step+1);
-            break;
+            return cur_pos.step+1;
         }
         for(int i=0; i<4; i++){
             int next_x = cur_pos.row+dir_x[i];
@@ -33,9 +24,24 @@ int main()
             if(A[next_x][next_y]==0){
                 if(!visited[next_x][next_y]){
                     q.push({next_x, next_y, cur_pos.step+1});
+                    visited[next_x][next_y]=true;
                 }
             }
         }
-
     }
+    return -1;
+}
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    int n, m, r, c;
+    cin>>n>>m>>r>>c;
+    for(int i=0; i<n; i++){
+        for(int j=0; j<m; j++){
+            cin>>A[i][j];
+        }
+    }
+    cout<<BFS(m,n,r,c);
 }
