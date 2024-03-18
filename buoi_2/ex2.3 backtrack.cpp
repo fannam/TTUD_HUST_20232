ae7cad2b29907307acdf2fe23ba4480c8b75de70
The Gardener <115874162+fannam@users.noreply.github.com>
2024-03-18 12:49:35 +0700

    Update ex2.3 backtrack.cpp

--- buoi_2/ex2.3 backtrack.cpp
+++ buoi_2/ex2.3 backtrack.cpp
@@ -1,11 +1,11 @@
 #include<bits/stdc++.h>
 using namespace std;
-vector<int>canTeach[11];//danh sach mon hoc giao vien i co the day 
-vector<int>conflict[31];//danh sach cac mon hoc trung gio voi mon hoc i 
+vector<int>canTeach[40];//danh sach mon hoc giao vien i co the day 
+vector<int>conflict[40];//danh sach cac mon hoc trung gio voi mon hoc i 
 int m, n;//m giao vien, n mon hoc
-int X[31]; //X[i] la giao vien day mon hoc i
+int X[40]; //X[i] la giao vien day mon hoc i
 int max_load = 1e9;
-
+int counter[40];
 bool check(int v, int p){//giao vien v, mon hoc p
 	//kiem tra gv co the day
 	bool can_teach_1 = false, can_teach_2 = true;
@@ -24,23 +24,22 @@ bool check(int v, int p){//giao vien v, mon hoc p
 	}
 	return can_teach_1 && can_teach_2;
 }
-void TRY(int p){//mon hoc p
-	if(p == n+1){
-		int counter[m];
-		for(int i=1; i<=n; i++){
-			counter[X[i]]++;
-		}
-		int max = -1e9;
-		for(int i=1; i<=m; i++){
-			if(max<counter[i]) max = counter[i];
-		}
-		max_load = min(max_load, max);
-	}
+bool check_max(int v, int p){
+	if(counter[v]<max_load-1) return true;
+	return false;
+}
+void TRY(int p, int cur_max_load){//mon hoc p
 	for(int v=1; v<=m; v++){//giao vien v
-		if(check(v, p)){
+		if(check(v, p) && check_max(v, p)){
 			X[p] = v;
-			TRY(p+1);
+			counter[v]++;
+			cur_max_load = max(cur_max_load, counter[v]);
+			if(p==n){
+				max_load = min(max_load, cur_max_load);
+			}
+			else TRY(p+1, cur_max_load);
 			X[p] = 0;
+			counter[v]--;
 		}
 	}
 	
@@ -65,7 +64,7 @@ int main(){
 		conflict[u].push_back(v);
 		conflict[v].push_back(u);
 	}
-	TRY(1);
+	TRY(1, 0);
 	if(max_load == 1e9) cout<<-1<<endl;
 	else cout<<max_load<<endl;
 }
